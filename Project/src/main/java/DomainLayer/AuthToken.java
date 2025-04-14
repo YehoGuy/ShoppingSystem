1f68ff4cfcbe499f950a92889cef42b67a5f00a3
yuval4604 <yuvalzar@post.bgu.ac.il>
2025-04-14 17:15:20 +0300

    Create AuthToken.java

---
+++ Project/src/main/java/DomainLayer/AuthToken.java
@@ -0,0 +1,17 @@
+public class AuthToken {
+    private String token; // The authentication token string
+    private long expirationTime; // The time when the token expires
+
+    public AuthToken(String token, long expirationTime) {
+        this.token = token;
+        this.expirationTime = expirationTime;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public long getExpirationTime() {
+        return expirationTime;
+    }
+}
