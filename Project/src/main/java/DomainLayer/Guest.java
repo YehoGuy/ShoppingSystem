f8efaa4a0b83065fb58223196196809e02787ce4
yuval4604 <yuvalzar@post.bgu.ac.il>
2025-04-14 17:21:38 +0300

    Create Guest.java

---
+++ Project/src/main/java/DomainLayer/Guest.java
@@ -0,0 +1,14 @@
+
+public class Guest extends User {
+    final private int guestId; // Unique identifier for the guest user
+
+    public Guest(int cartId, int guestId, String sessionId, String ipAddress) {
+        super(cartId); // Call the constructor of the User class
+        this.guestId = guestId; // Initialize guest ID
+    }
+
+    public int getGuestId() {
+        return guestId; // Return the guest ID
+    }
+    
+}
