7361f61e9548dc27e30a4a76d30db728f237a48e
YehoGuy <yehowork23@gmail.com>
2025-04-28 21:43:31 +0300

    add Reciept class

---
+++ Project/src/main/java/DomainLayer/Purchase/Reciept.java
@@ -0,0 +1,156 @@
+package DomainLayer.Purchase;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+/**
+ * This Data Object class represents a receipt for a purchase.
+ * It contains information about the items purchased, their prices, the purchase status etc...
+ */
+public class Reciept {
+    protected final int purchaseId;                     // purchase ID
+    protected final int userId;                        // initiating user ID
+    protected final int storeId;                      // store ID
+    protected final Map<Integer, Integer> items; // itemId -> quantity
+    protected final Address shippingAddress;              // shipping address
+    protected final boolean isCompleted;                 // purchase status   
+    protected final LocalDateTime timeOfCompletion;     // time of purchase completion
+
+    // very important to make sure a reciept is concurrent עכשווית
+    protected final LocalDateTime timestampOfRecieptGeneration; // time of receipt generation!!
+
+    /**
+     * Constructs a new {@code Reciept} with the specified user ID, store ID, and items.
+     * this constructor is meant for UNCOMPLETED purchases
+     *
+     * @param purchaseId the ID of the purchase.
+     * @param userId the ID of the user initiating the purchase.
+     * @param storeId the ID of the store where the purchase is made.
+     * @param items a map of item IDs to their quantities.
+     * @param shippingAddress the shipping address for the purchase.
+     */
+    public Reciept(int purchaseId, int userId, int storeId, Map<Integer, Integer> items, Address shippingAddress) {
+        this.purchaseId = purchaseId;
+        this.userId = userId;
+        this.storeId = storeId;
+        this.items = Map.copyOf(items);
+        this.shippingAddress = shippingAddress;
+        this.isCompleted = false;
+        this.timeOfCompletion = null;
+        this.timestampOfRecieptGeneration = LocalDateTime.now();
+    }
+
+    
+    /**
+     * Constructs a new {@code Reciept} with the specified user ID, store ID, and items.
+     * this constructor is meant for UNCOMPLETED purchases
+     *
+     * @param purchaseId the ID of the purchase.
+     * @param userId the ID of the user initiating the purchase.
+     * @param storeId the ID of the store where the purchase is made.
+     * @param items a map of item IDs to their quantities.
+     * @param shippingAddress the shipping address for the purchase.
+     */
+    public Reciept(int purchaseId, int userId, int storeId, Map<Integer, Integer> items, Address shippingAddress, LocalDateTime timeOfCompletion) {
+        this.purchaseId = purchaseId;
+        this.userId = userId;
+        this.storeId = storeId;
+        this.items = Map.copyOf(items);
+        this.shippingAddress = shippingAddress;
+        this.isCompleted = true;
+        this.timeOfCompletion = timeOfCompletion;
+        this.timestampOfRecieptGeneration = LocalDateTime.now();
+    }
+
+    /**
+     * Returns the ID of the purchase.
+     *
+     * @return the purchase ID.
+     */
+    public int getPurchaseId() {
+        return purchaseId;
+    }
+    /**
+     * Returns the ID of the user who initiated the purchase.
+     *
+     * @return the user ID.
+     */
+    public int getUserId() {
+        return userId;
+    }
+    /**
+     * Returns the ID of the store where the purchase was made.
+     *
+     * @return the store ID.
+     */
+    public int getStoreId() {
+        return storeId;
+    }
+    /**
+     * Returns a map of item IDs to their quantities in the purchase.
+     *
+     * @return a {@code HashMap} containing the items and their quantities.
+     */
+    public Map<Integer, Integer> getItems() {
+        // items is immutable, so we can return it directly
+        return items;
+    }
+
+    /**
+     * Returns the shipping address for the purchase.
+     *
+     * @return the shipping address.
+     */
+    public String getAddress() {
+        return shippingAddress.toString();
+    }
+
+    /**
+     * Returns whether the purchase is completed.
+     *
+     * @return {@code true} if the purchase is completed, {@code false} otherwise.
+     */
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    /**
+     * Returns the time of purchase completion.
+     *
+     * @return the time of purchase completion.
+     */
+    public LocalDateTime getTimeOfCompletion() {
+        return timeOfCompletion;
+    }
+
+    /**
+     * Returns the time of receipt generation.
+     *
+     * @return the time of receipt generation.
+     */
+    public LocalDateTime getTimestampOfRecieptGeneration() {
+        return timestampOfRecieptGeneration;
+    }
+
+    /**
+     * Returns a string representation of the receipt.
+     *
+     * @return a string representation of the receipt.
+     */
+    @Override
+    public String toString() {
+        return "Reciept{" +
+                "purchaseId=" + purchaseId +
+                ", userId=" + userId +
+                ", storeId=" + storeId +
+                ", items=" + items +
+                ", shippingAddress=" + shippingAddress +
+                ", isCompleted=" + isCompleted +
+                ", timeOfCompletion=" + timeOfCompletion +
+                ", timestampOfRecieptGeneration=" + timestampOfRecieptGeneration +
+                '}';
+    }
+    
+
+
+}
