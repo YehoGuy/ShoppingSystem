7d1837b0070d3736f3803e09b5728281d4b3b295
roeeBarOz <broee@post.bgu.ac.il>
2025-05-06 15:57:19 +0300

    rearranged dependencies and added main file to UI

---
+++ Project/src/main/java/UI/mainUI.java
@@ -0,0 +1,11 @@
+package UI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class mainUI {
+    public static void main(String[] args) {
+        SpringApplication.run(mainUI.class, args);
+    }
+}
