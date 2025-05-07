b1a324ff0a989ffdac12bfc6c6c2e3dfae454347
roeeBarOz <broee@post.bgu.ac.il>
2025-05-07 17:25:14 +0300

    removed import

--- Project/src/main/java/UI/ShopSearchView.java
+++ Project/src/main/java/UI/ShopSearchView.java
@@ -13,7 +13,6 @@ import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 
 import DTOs.ItemDTO;
 import DTOs.ShopDTO;
