34f44832040437f3b1f36d611baeb345e659c50b
yuval4604 <yuvalzar@post.bgu.ac.il>
2025-05-07 14:08:14 +0300

    Update ItemDTO.java

--- Project/src/main/java/DTOs/ItemDTO.java
+++ Project/src/main/java/DTOs/ItemDTO.java
@@ -3,14 +3,18 @@ package DTOs;
 import DomainLayer.Item.ItemCategory;
 
 public class ItemDTO {
+    private int id;
+    private double price;
     private String name;
     private String description;
     private ItemCategory category;
 
-    public ItemDTO(String name, String description, double price) {
+    public ItemDTO(int id, String name, String description, double price, ItemCategory category) {
         this.name = name;
         this.description = description;
         this.category = category;
+        this.price = price;
+        this.id = id;
     }
 
     public String getName() {
@@ -23,6 +27,13 @@ public class ItemDTO {
 
     public String getCategory() {
         return category.toString();
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public int getId() {
+        return id;
     }
 
 }
