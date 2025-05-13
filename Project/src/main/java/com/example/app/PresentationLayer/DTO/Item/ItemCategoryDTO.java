fd8dcea705c4936e01976f2ed051b224e984a913
YehoGuy <yehowork23@gmail.com>
2025-05-13 12:43:24 +0300

    no need for ItemCategoryDTO since Category is just an ENUM

--- Project/src/main/java/com/example/app/PresentationLayer/DTO/Item/ItemCategoryDTO.java
+++
@@ -1,8 +0,0 @@
-package com.example.app.PresentationLayer.DTO.Item;
-
-public record ItemCategoryDTO(String name) {
-
-    public static ItemCategoryDTO fromDomain(com.example.app.DomainLayer.Item.ItemCategory c) {
-        return new ItemCategoryDTO(c.name());
-    }
-}
