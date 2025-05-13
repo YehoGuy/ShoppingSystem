ac4c015471d715e536cceb7e6cf140e0396a0217
roeeBarOz <broee@post.bgu.ac.il>
2025-05-13 09:25:18 +0300

    added before event to home view. it will check first that the user is connected

--- Frontend/src/main/java/UI/HomeView.java
+++ Frontend/src/main/java/UI/HomeView.java
@@ -1,17 +1,26 @@
 package UI;
 
+import javax.swing.GroupLayout.Alignment;
+
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.VaadinSession;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 
 @Route(value = "home", layout = AppLayoutBasic.class)
 
-public class HomeView extends VerticalLayout {
-    public HomeView() {
+public class HomeView extends VerticalLayout implements BeforeEnterObserver {
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
         if (VaadinSession.getCurrent().getAttribute("authToken") == null) {
-            UI.getCurrent().navigate("login");
+            event.forwardTo("");
         }
+    }
+
+    public HomeView() {
         setSizeFull();
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
