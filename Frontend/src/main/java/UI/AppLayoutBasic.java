4600631c447b6a5532178e3618682846d83086a0
Ori86 <ori.eines8@gmail.com>
2025-06-21 22:59:39 +0300

    our id is always on the pannel and bids and auctions are admin view only:)

--- Frontend/src/main/java/UI/AppLayoutBasic.java
+++ Frontend/src/main/java/UI/AppLayoutBasic.java
@@ -11,6 +11,7 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.theme.lumo.LumoUtility;
@@ -40,21 +41,25 @@ public class AppLayoutBasic extends AppLayout implements RouterLayout {
     }
 
     private SideNav getSideNav() {
+        String title = "my id:"+getUserId();
         SideNav sideNav = new SideNav();
+        sideNav.setLabel(title);
         sideNav.addItem(
                 new SideNavItem("Home", "/home", VaadinIcon.HOME.create()),
                 new SideNavItem("Profile", "/profile", VaadinIcon.USER.create()),
                 new SideNavItem("Shopping Cart", "/cart", VaadinIcon.CART.create()),
                 new SideNavItem("Search Item", "/items", VaadinIcon.SEARCH.create()),
                 new SideNavItem("Search Shop", "/shops", VaadinIcon.SHOP.create()),
-                new SideNavItem("Bids", "/bids", VaadinIcon.MONEY.create()),
-                new SideNavItem("Auctions", "/auctions", VaadinIcon.GAVEL.create()),
+                // new SideNavItem("Bids", "/bids", VaadinIcon.MONEY.create()),
+                // new SideNavItem("Auctions", "/auctions", VaadinIcon.GAVEL.create()),
                 new SideNavItem("My Shops", "/myshops", VaadinIcon.LIST_UL.create()),
                 new SideNavItem("My Messages", "/messages", VaadinIcon.ENVELOPE.create()));
 
         // only for admins
         Boolean isAdmin = (Boolean) VaadinSession.getCurrent().getAttribute("isAdmin");
         if (Boolean.TRUE.equals(isAdmin)) {
+            sideNav.addItem(new SideNavItem("Bids", "/bids", VaadinIcon.MONEY.create()));
+            sideNav.addItem(new SideNavItem("Auctions", "/auctions",  VaadinIcon.GAVEL.create()));
             sideNav.addItem(new SideNavItem("Admin Panel", "/admin", VaadinIcon.SHIELD.create()));
         }
 
